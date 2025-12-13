f0fbe1de0b5e86533c5c2bcdafc6f9ee04a722a8
nurislam123321-pixel <nurislam.123321@gmail.com>
2025-12-13 20:54:57 +0600

    Update script.js

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,5 +1,5 @@
 const response = await fetch(
-  "https://us-central1-ai-content-generator-481106.cloudfunctions.net/generate-ai",
+  "https://us-central1-ai-content-generator-481106.cloudfunctions.net",
   {
     method: "POST",
     headers: {
